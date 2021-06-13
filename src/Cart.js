dc1960a030b005dd3c22e1e824bd720188d6e2a7
Selmane Lamia <hl_selmane@esi.dz>
2021-06-13 16:07:07 +0100

    icon added

--- src/Cart.js
+++ src/Cart.js
@@ -1,7 +1,31 @@
 import React from "react";
 import Table from "react-bootstrap/Table";
+import NavBar from "./NavBar";
 
 export default function Cart() {
-  return;
-  <div></div>;
+  return (
+    <div>
+      <NavBar />
+      <div className="cart-container">
+        <Table striped bordered>
+          <thead style={{ background: "white" }}>
+            <tr>
+              <th></th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tr></tr>
+          <tbody>
+            <tr>
+              <td>3</td>
+              <td colSpan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+    </div>
+  );
 }
