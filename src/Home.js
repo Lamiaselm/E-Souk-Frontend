7aeba6acededd7032df428342ce8f0cbfb30d952
Selmane Lamia <hl_selmane@esi.dz>
2021-06-13 12:33:29 +0100

    added search bar

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,11 @@ import Card from "react-bootstrap/Card";
 import imprimante from "./Assets/canon-imprimante-pixma-ts3350-multifonction-jet-d.jpeg";
 import mac from "./Assets/macbook-pro-2017.jpeg";
 import baffe from "./Assets/9179UILdcKL._AC_SS450_.jpg";
+import mouse from "./Assets/41VkzplsASL._SL500_.jpg";
+import f1 from "./Assets/Robe-habillee-fille-marine-ROFEFI005-big.jpeg";
+import f2 from "./Assets/ec092d1c550231d2be6c03330d946832.jpeg";
+import g1 from "./Assets/51aR8tzdoLL._AC_SX425_.jpg";
+import g2 from "./Assets/7541080-227.jpeg";
 
 export default function Home() {
   return (
@@ -48,7 +53,7 @@ export default function Home() {
           </Card.Body>
         </Card>
         <Card className="card">
-          <Card.Img variant="top" src={imprimante} style={{ width: "75%" }} />
+          <Card.Img variant="top" src={mouse} style={{ width: "75%" }} />
           <Card.Body>
             <Card.Title>Imprimante Canon</Card.Title>
             <Card.Text>
@@ -58,10 +63,48 @@ export default function Home() {
             <Button variant="warning">Detail</Button>
           </Card.Body>
         </Card>
+      </div>
+      <div className="category">
+        <h3>Vêtements Enfant</h3>
+      </div>
+      <div className="card-container">
         <Card className="card">
-          <Card.Img variant="top" src={imprimante} style={{ width: "75%" }} />
+          <Card.Img variant="top" src={f1} style={{ width: "100%" }} />
           <Card.Body>
-            <Card.Title>Imprimante Canon</Card.Title>
+            <Card.Title>Robe marine</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="warning">Detail</Button>
+          </Card.Body>
+        </Card>
+        <Card className="card">
+          <Card.Img variant="top" src={f2} style={{ width: "100%" }} />
+          <Card.Body>
+            <Card.Title>Ensemle été</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="warning">Detail</Button>
+          </Card.Body>
+        </Card>
+        <Card className="card">
+          <Card.Img variant="top" src={g1} style={{ width: "75%" }} />
+          <Card.Body>
+            <Card.Title>Ensemble classique</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="warning">Detail</Button>
+          </Card.Body>
+        </Card>
+        <Card className="card">
+          <Card.Img variant="top" src={g2} style={{ width: "75%" }} />
+          <Card.Body>
+            <Card.Title>Ensemble été</Card.Title>
             <Card.Text>
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
+import { Nav, Navbar, Container, Form, FormControl } from "react-bootstrap";
 import logo from "./Assets/e-souk-logo.png";
 import Button from "react-bootstrap/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -9,10 +9,21 @@ export default function NavBar() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="light" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand href="#home" style={{ marginRight: "40px" }}>
           <img className="logo-nav" src={logo} />
         </Navbar.Brand>
+
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Form className="d-flex">
+          <FormControl
+            style={{ width: "300px" }}
+            type="search"
+            placeholder="Search"
+            className="mr-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-dark">Search</Button>
+        </Form>
         <Navbar.Collapse className="justify-content-end">
           <Nav>
             <Button className="nav-button" variant="outline-warning">
