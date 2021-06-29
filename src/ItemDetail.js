94a3553a19b195297376e99dd583dff2fd8d8143
Selmane Lamia <hl_selmane@esi.dz>
2021-06-29 22:41:27 +0100

    changes

--- src/ItemDetail.js
+++ src/ItemDetail.js
@@ -42,7 +42,7 @@ export default function ItemDetail() {
               Le lorem ipsum est, en imprimerie, une suite de mots sans
               signification utilisée à titre provisoire pour calibrer une mise
               en page, le texte définitif venant remplacer le faux-texte dès
-              qu'il est prêt ou que la mise{" "}
+              qu'il est prêt ou que la mise
             </p>
             <p>
               Le lorem ipsum est, en imprimerie, une suite de mots sans
